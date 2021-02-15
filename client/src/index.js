b703b0ace3c9cfcf1e1c8dee6e907faa1f068df6
Javier Zarate <javalzar17@gmail.com>
2021-02-14 16:40:38 -0800

    react webpack set up

---
+++ client/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => <div>Hello React is Live</div>;
+ReactDOM.render(<App/>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, '/client/src', 'index.js'),
+  output: {
+    path: path.join(__dirname, '/client/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        include: path.join(__dirname, '/client/src'),
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                "targets": "defaults"
+              }],
+              '@babel/preset-react'
+            ]
+          }
+        }]
+      }
+    ]
+  },
+  mode: "development"
+}
